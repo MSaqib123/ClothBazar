d7821ffbafbdf0884cdb0182481340c141e01b5d
MSaqib123 <m43577535@gmail.com>
2023-05-01 09:52:04 +0500

    CRUD on Category

---
+++ ClothBaza.Services/CategoriesService.cs
@@ -0,0 +1,65 @@
+﻿using ClothBazar.Database;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBaza.Services
+{
+    public class CategoriesService
+    {
+        //____ create ______
+        public void Save(Category model)
+        {
+            using (var context = new CBContext())
+            {
+                context.categories.Add(model);
+                context.SaveChanges();
+            }
+        }
+
+        //_____ List ________
+        public List<Category> GetList()
+        {
+            using (var context = new CBContext())
+            {
+                return context.categories.ToList();
+            }
+        }
+        
+        //_____ Single Record ________
+        public Category GetSingleRecord(int Id)
+        {
+            using (var context = new CBContext())
+            {
+                return context.categories.Find(Id);
+            }
+        }
+
+
+        //_____ Update Record ________
+        public void Update(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(category).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        //_____ Delete Record ________
+        public void Delete(Category category)
+        {
+            using (var context = new CBContext())
+            {
+                context.Entry(category).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
---
+++ ClothBazar.Web/Controllers/CategoryController.cs
@@ -0,0 +1,67 @@
+﻿using ClothBaza.Services;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesService service = new CategoriesService();
+
+        public ActionResult Index()
+        {
+            var list = service.GetList();
+            return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category model)
+        {
+            service.Save(model);
+            return View(model);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var record = service.GetSingleRecord(id);
+            return View(record);
+        }
+        [HttpPost]
+        public ActionResult Edit(Category model)
+        {
+            service.Update(model);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var record = service.GetSingleRecord(id);
+            return View(record);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var record = service.GetSingleRecord(id);
+            return View(record);
+        }
+        [HttpPost]
+        public ActionResult Delete(Category model)
+        {
+            service.Delete(model);
+            return RedirectToAction("Index");
+        }
+
+
+        
+
+    }
+}
