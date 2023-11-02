99504f38213a924642ee1a928291901bc1a980be
MBoota <m43577535@gmail.com>
2023-11-02 23:52:33 +0500

    adding_DashboardView

---
+++ ClothBazar.Web/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using ClothBaza.Services;
+using ClothBazar.Entities;
+using ClothBazar.Web.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //CategoriesService service = new CategoriesService();
+        public ActionResult Index()
+        {
+            HomeVM vm = new HomeVM();
+            vm.FeatureCategoryList = CategoriesService.Instance.GetFeaturedList();
+            return View(vm);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
